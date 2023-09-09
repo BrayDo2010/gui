f915d519ff1ac2e18a7b174c5e8f8aad8a935d93
BrayDo2010 <113742627+BrayDo2010@users.noreply.github.com>
2023-09-09 17:32:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('https://raw.githubusercontent.com/BrayDo2010/gui/main/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
